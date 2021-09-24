dc585857fe2ca12e00af4e50d378476aad895ef3
Umineco <78210151+umineco@users.noreply.github.com>
2021-09-25 08:33:55 +0900

    Add files via upload

---
+++ article-test01.js
@@ -0,0 +1,43 @@
+var ogurl ="https://umineco.github.io/blog/article-test01"; //OGP:URL
+var ogimage ="https://github.com/umineco/umineco.github.io/blob/main/image/thumbnail-1.png"; //OGP:画像
+var secpath ="ブログ"; //第2パス
+var secpathurl ="/blog"; //第2パスURL
+var articletitle ="記号論理と圏";//記事タイトル
+var datepub ="2021/9/23 - "; //発行日時
+var articlenext ="次の記事" //次の記事
+var articlenexturl ="/blog/next" //次の記事URL
+var articleprev ="前の記事" //前の記事
+var articleprevurl ="/blog/prev" //前の記事URL
+
+
+document.getElementById("og-url").setAttribute("content",ogurl)
+
+document.getElementById("og-type").setAttribute("content","article") //OGP:タイトル
+
+document.getElementById("og-image").setAttribute("content",ogimage)
+
+document.getElementById("page-title").innerHTML =articletitle+" - "+blogtitle //ページタイトル
+
+document.getElementById("path-2").innerHTML =secpath
+document.getElementById("path-2").setAttribute("href",secpathurl)
+
+var artit =document.getElementsByClassName("article-title");
+for(i=0; i<artit.length; i++){artit[i].innerHTML =articletitle;}
+
+document.getElementById("date-published").innerHTML =datepub
+
+var chg =new Date(document.lastModified);
+var year =chg.getFullYear();
+var month =chg.getMonth()+1;
+var date =chg.getDate();
+var datechg =year+"/"+month+"/"+date;
+document.getElementById("date-changed").innerHTML =datechg //最終更新日
+
+document.getElementById("article-next").innerHTML =articlenext
+document.getElementById("article-next").setAttribute("href",articlenexturl)
+
+document.getElementById("article-prev").innerHTML =articleprev
+document.getElementById("article-prev").setAttribute("href",articleprevurl)
+
+var copyrightyear =document.getElementById("copyright-year");
+if(year>2021){copyrightyear.innerHTML ="-"+year}
