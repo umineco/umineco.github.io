1693769d1340c87c562c1921a910ae98eccb07c2
Umineco <78210151+umineco@users.noreply.github.com>
2021-09-26 00:14:05 +0900

    Add files via upload

--- article-test01.js
+++ article-test01.js
@@ -1,25 +1,15 @@
 var ogurl ="https://umineco.github.io/blog/article-test01"; //OGP:URL
 var ogimage ="https://raw.githubusercontent.com/umineco/umineco.github.io/main/image/thumbnail-1.png"; //OGP:画像
-var pagetitle =articletitle+" - "+blogtitle; //ページタイトル
 var secpath ="ブログ"; //第2パス
 var secpathurl ="/blog"; //第2パスURL
 var articletitle ="記号論理と圏";//記事タイトル
+var pagetitle =articletitle+" - "+blogtitle; //ページタイトル
 var datepub ="2021/9/23 - "; //発行日時
 var articlenext ="次の記事" //次の記事
 var articlenexturl ="/blog/next" //次の記事URL
 var articleprev ="前の記事" //前の記事
 var articleprevurl ="/blog/prev" //前の記事URL
 
-
-document.getElementById("og-url").setAttribute("content",ogurl)
-
-document.getElementById("og-title").setAttribute("content",pagetitle) //OGP:タイトル
-
-document.getElementById("og-type").setAttribute("content","article")
-
-document.getElementById("og-image").setAttribute("content",ogimage)
-
-document.getElementById("page-title").innerHTML =pagetitle
 
 document.getElementById("path-2").innerHTML =secpath
 document.getElementById("path-2").setAttribute("href",secpathurl)
