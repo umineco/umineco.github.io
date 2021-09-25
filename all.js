d49417f4040f1f3812ceda1feb89f13948c53ade
Umineco <78210151+umineco@users.noreply.github.com>
2021-09-26 08:02:27 +0900

    Delete all.js

--- all.js
+++
@@ -1,9 +0,0 @@
-var blogtitle ="Empty Necoset"; //ブログタイトル
-var webmaster ="鴎海ねこ"; //管理者
-
-
-var bltit =document.getElementsByClassName("blog-title");
-for(i=0; i<bltit.length; i++){bltit[i].innerHTML =blogtitle;}
-
-var webm =document.getElementsByClassName("webmaster");
-for(i=0; i<webm.length; i++){webm[i].innerHTML =webmaster;}
