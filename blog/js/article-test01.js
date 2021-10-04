7c0b10346091f56191a28d4130a909c30cbd6239
Umineco <78210151+umineco@users.noreply.github.com>
2021-10-04 20:42:16 +0900

    Add files via upload

--- blog/js/article-test01.js
+++ blog/js/article-test01.js
@@ -4,9 +4,9 @@ var articletitle ="圏同値"
 var pagetitle =articletitle+" - "+blogtitle; //ページタイトル
 var datepub ="2021/9/23 - "; //発行日時
 var articlenext ="次の記事"; //次の記事
-var articlenexturl ="next.html"; //次の記事URL
+var articlenexturl ="/blog/next"; //次の記事URL
 var articleprev ="前の記事"; //前の記事
-var articleprevurl ="prev.html"; //前の記事URL
+var articleprevurl ="/blog/prev"; //前の記事URL
 
 document.getElementById("page-title").innerHTML =pagetitle
 
